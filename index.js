6e734699db75c4f7b24ed9af7437324c9681e610
denismnjima <denismnjima@gmail.com>
2024-07-10 11:32:18 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // code your solution here
-const saturdayFun = ()=>{
-    
+const saturdayFun = (activity='roller-skate')=>{
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun
+
+const mondayWork = (activity="go to the office")=>{
+    return `This Monday, I will ${activity}.`
+}
+mondayWork
+const wrapAdjective =(wrap)=>{
+    return (wrapAdjective)=>{
+        return `You are ${wrap}${wrapAdjective}${wrap}!`
+
+    }
 }
